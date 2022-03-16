615e2b257a2ebe7b23c5099629c8bf47498e9209
Cristy <61757856+Cristy-html@users.noreply.github.com>
2022-03-16 11:52:26 +0100

    More ...

---
+++ 2/script.py
@@ -0,0 +1,11 @@
+import pyautogui as  p
+import  time
+
+time.sleep(1)
+
+for i in rnage (50):
+    p.click(500, 1010)
+    time.sleep(0.5)
+    p.write("Rispondi Infame")
+    time.sleep(0.5)
+    p.press('Enter')
